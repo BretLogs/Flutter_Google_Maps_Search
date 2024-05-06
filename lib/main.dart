824595ec754f82336cf2d6f6dae4aab98c6f8d64
Israel Breta <i_israel.breta@stratpoint.com>
2024-05-06 10:45:18 +0800

    ALL DONE!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,58 +12,69 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
+  GoogleMapController? mapController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.black,
       ),
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            // SearchMapPlaceWidget(
+            //     hasClearButton: true,
+            //     placeType: PlaceType.address,
+            //     placeholder: "Enter the location",
+            //     apiKey: "AIzaSyBwoZpl1lHZXTvbjet8P-wsZo5Qtpf_Kzk",
+            //     onSelected: (Place place) async {
+            //       Geolocation geolocation = await place.geolocation;
+            //       mapController.animateCamera(
+            //         CameraUpdate.newLatLng(geolocation.getcoordinates),
+            //       );
+            //       mapController.animateCamera(
+            //         CameraUpdate.newLatLngBound(geolocation.bounds, 0),
+            //       );
+            //     }),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 500,
+                child: GoogleMap(
+                  onMapCreated: (GoogleMapController googleMapController) {
+                    setState(() {
+                      mapController = googleMapController;
+                    });
+                  },
+                  initialCameraPosition: const CameraPosition(
+                    zoom: 15.0,
+                    target: LatLng(21.1458, 79.08822),
+                  ),
+                  mapType: MapType.normal,
+                ),
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
